37d3573b1eed7e7dcd75f6f78c141f9cc3fcd200
ferris <jake@outracks.com>
2014-12-15 03:27:23 +0100

    Slowly learning FParsec :)

--- FFlat.Compiler/Proto.fsx
+++ FFlat.Compiler/Proto.fsx
@@ -1,1 +1,16 @@
-﻿
+﻿#r "../packages/FParsec.1.0.1/lib/net40-client/FParsecCS.dll"
+#r "../packages/FParsec.1.0.1/lib/net40-client/FParsec.dll"
+
+open FParsec
+
+type Expression =
+    | IntLiteral of int
+
+let code = @"80085"
+
+let integer = pint32
+let expression = integer |>> IntLiteral
+
+match run expression code with
+| Success (result, _, _) -> printfn "Expression: %A" result
+| Failure (message, _, _) -> printfn "Failure: %s" message
