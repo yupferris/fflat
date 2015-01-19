cebd065e38b37569a02c4c965964054754ce0f25
ferris <jake@outracks.com>
2015-01-19 10:51:22 +0100

    and there's a proper constructor :)

--- FFlat.Compiler/Codegen.fs
+++ FFlat.Compiler/Codegen.fs
@@ -28,16 +28,17 @@
         | Add -> OpCodes.Add
         | Mul -> OpCodes.Mul
 
+    let emitLoadArg (ilg : ILGenerator) = function
+        | 0 -> ilg.Emit(OpCodes.Ldarg_0)
+        | 1 -> ilg.Emit(OpCodes.Ldarg_1)
+        | 2 -> ilg.Emit(OpCodes.Ldarg_2)
+        | 3 -> ilg.Emit(OpCodes.Ldarg_3)
+        | x -> ilg.Emit(OpCodes.Ldarg, x)
+
     let rec exprCodegen (ilg : ILGenerator) = function
         | IlUnitLiteral -> ()
         | IlIntLiteral (value) -> ilg.Emit(OpCodes.Ldc_I4, value)
-        | IlArgumentReference (index, _) ->
-            match index with
-            | 0 -> ilg.Emit(OpCodes.Ldarg_0)
-            | 1 -> ilg.Emit(OpCodes.Ldarg_1)
-            | 2 -> ilg.Emit(OpCodes.Ldarg_2)
-            | 3 -> ilg.Emit(OpCodes.Ldarg_3)
-            | x -> ilg.Emit(OpCodes.Ldarg, x)
+        | IlArgumentReference (index, _) -> emitLoadArg ilg index
         | IlBinOpExpression (op, left, right) ->
             exprCodegen ilg left
             exprCodegen ilg right
@@ -46,15 +47,19 @@
     let declCodegen (moduleBuilder : ModuleBuilder) (typeBuilder : TypeBuilder) = function
         | IlRecord (name, members) ->
             let recordTypeBuilder = moduleBuilder.DefineType(name, TypeAttributes.Public)
+
+            // Generate backing fields
             let fieldBuilders =
                 members
                 |> List.map (fun x ->
                     (x, recordTypeBuilder.DefineField(
                         "_" + x.name, ilTypeToMsilType x.type', FieldAttributes.Private)))
                 |> Map.ofList
+
+            // Generate getter properties for backing fields
             fieldBuilders
             |> Map.iter (fun member' fieldBuilder ->
-                let getterType = ilTypeToMsilType member'.type'
+                let getterType = fieldBuilder.FieldType
                 let getterPropBuilder =
                     recordTypeBuilder.DefineProperty(
                         member'.name,
@@ -73,6 +78,27 @@
                 ilg.Emit(OpCodes.Ldfld, fieldBuilder)
                 ilg.Emit(OpCodes.Ret)
                 getterPropBuilder.SetGetMethod(getter))
+
+            // Generate proper constructor
+            let ctor =
+                recordTypeBuilder.DefineConstructor(
+                    MethodAttributes.Public,
+                    CallingConventions.Standard,
+                    members
+                    |> List.map (fun x -> (Map.find x fieldBuilders).FieldType)
+                    |> Array.ofList)
+            let ilg = ctor.GetILGenerator()
+            ilg.Emit(OpCodes.Ldarg_0)
+            let objCtor = typeof<obj>.GetConstructor(Type.EmptyTypes)
+            ilg.Emit(OpCodes.Call, objCtor)
+            members
+            |> List.iteri (fun i x ->
+                let fieldBuilder = Map.find x fieldBuilders
+                ilg.Emit(OpCodes.Ldarg_0)
+                emitLoadArg ilg (i + 1)
+                ilg.Emit(OpCodes.Stfld, fieldBuilder))
+            ilg.Emit(OpCodes.Ret)
+
             recordTypeBuilder.CreateType() |> ignore
         | IlFunction (name, parameters, returnType, expr) ->
             let methodBuilder =
